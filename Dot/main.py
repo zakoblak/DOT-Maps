b0d55ecda61e7bd65cf11b234bc4b99c4f236112
zakoblak <zakoblak122@gmail.com>
2025-05-05 20:01:44 +0200

    Add files via upload

--- Dot/main.py
+++ Dot/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from tinydb import TinyDB, Query
+import os
 
 app = Flask(__name__)
 app.secret_key = 'skrivni_kljuc'
@@ -7,6 +8,9 @@ app.secret_key = 'skrivni_kljuc'
 db = TinyDB('uporabniki.json')
 users = db.table('users')
 User = Query()
+
+points_db = TinyDB('točke.json')
+points_table = points_db.table('points')
 
 @app.route('/')
 def domov():
@@ -43,6 +47,41 @@ def register():
         return redirect(url_for('domov'))
 
     return render_template('registracija.html')
+
+@app.route('/save_point', methods=['POST'])
+def save_point():
+    """
+    Shrani točko z informacijami, ki jih uporabnik vnese.
+    """
+    point_data = request.json
+    points_table.insert(point_data)
+    return jsonify({"status": "success", "message": "Točka uspešno shranjena!"}), 200
+
+@app.route('/get_points', methods=['GET'])
+def get_points():
+    """
+    Pridobi vse shranjene točke in jih vrne v GeoJSON formatu.
+    """
+    points = points_table.all()
+    geojson = {"type": "FeatureCollection", "features": []}
+    for point in points:
+        geojson["features"].append({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [point['lon'], point['lat']]
+            },
+            "properties": point['properties']
+        })
+    return jsonify(geojson)
+
+@app.route('/onas')
+def onas():
+    return render_template('onas.html')
+
+@app.route('/kontak')
+def kontak():
+    return render_template('kontak.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
